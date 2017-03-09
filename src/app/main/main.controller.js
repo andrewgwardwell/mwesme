80f1bf1f015e5bd21df5593d3f74a00db4037368
BigEngland <andrewgwardwell@gmail.com>
2017-03-09 11:46:37 -0500

    everything else

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,37 @@
+export class MainController {
+  constructor ($timeout, webDevTec, toastr) {
+    'ngInject';
+
+    this.awesomeThings = [];
+    this.classAnimation = '';
+    this.creationDate = 1488993647579;
+    this.toastr = toastr;
+    this.middle = '';
+
+    this.activate($timeout, webDevTec);
+  }
+
+  activate($timeout, webDevTec) {
+    this.getWebDevTec(webDevTec);
+    $timeout(() => {
+      this.classAnimation = 'rubberBand';
+    }, 4000);
+  }
+
+  getWebDevTec(webDevTec) {
+    this.awesomeThings = webDevTec.getTec();
+
+    angular.forEach(this.awesomeThings, (awesomeThing) => {
+      awesomeThing.rank = Math.random();
+    });
+  }
+
+  showToastr() {
+    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+    this.classAnimation = '';
+  }
+
+  toggleStates(state) {
+    this.middle = state;
+  }
+}
